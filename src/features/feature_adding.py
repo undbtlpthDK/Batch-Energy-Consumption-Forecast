c3f700259f1438902732b776de07e752807b0731
undbtlpthDK <daniils.krivosenoks@gmail.com>
2026-01-31 08:04:58 +0200

    add lagged consumption and generation

--- src/features/feature_adding.py
+++ src/features/feature_adding.py
@@ -3,7 +3,57 @@ import pandas as pd
 from feature_utils import is_lv_holiday, is_weekend
 
 
+def add_lagged_energy_values(df: pd.DataFrame) -> pd.DataFrame:
+    """Adds the lagged energy consumption and generation features to DataFrame
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Smart Meters reading DataFrame where
+        energy_import_kwh and energy_export_kwh columns should exists
+    Returns
+    -------
+    pd.DataFrame
+        Enriched DataFrame with energy related lags
+    """
+    # Specifies the range of lags applied
+    lags = [1, 2, 3, 4, 5, 24, 48, 168]
+
+    df_lagged = df.sort_values(["object_id", "timestamp"]).copy()
+
+    for lag in lags:
+        shifted = (
+            df_lagged
+            .groupby("object_id")[["energy_import_kwh", "energy_export_kwh"]]
+            .shift(lag)
+        )
+
+        df_lagged[[f"import_lag_{lag}", f"export_lag_{lag}"]] = shifted
+
+    return df_lagged
+
+
 def add_calendar_features(df: pd.DataFrame) -> pd.DataFrame:
+    """Add the calendar values based on "timestamp" to DataFrame
+
+    columns added:
+        date
+        hour, hour_sin, hour_coos of the day
+        week, week_sin, week_coos of the year
+        weekday, weekday_sin, weekday_coos of the week
+        is_weekend, is_holiday
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Smart Meters reading DataFrame with "timestamp" column
+
+    Returns
+    -------
+    pd.DataFrame
+        Enriched DataFrame
+    """
+    df = _add_dates(df)
     df = _add_hours(df)
     df = _add_weeks(df)
     df = _add_weekdays(df)
