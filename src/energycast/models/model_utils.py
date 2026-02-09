2b82ce3a72af287b1fcbe783a502fa728596ee19
undbtlpthDK <daniils.krivosenoks@gmail.com>
2026-02-09 10:54:52 +0200

    func: add functionality to load model configuration from yaml

---
+++ src/energycast/models/model_utils.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import yaml
+
+ROOT_DIR = Path(__file__).resolve().parents[3]
+MODEL_CONFIG_DIR = ROOT_DIR / "configs" / "models"
+
+
+@dataclass
+class ModelConfig:
+    # data
+    train_set: str
+    dev_set: str
+    test_set: str
+    id_col: str
+    time_col: str
+
+    # model
+    model_type: str
+    strategy: str
+    horizon: int
+    target_col: str
+    features: list[str]
+    model_params: dict
+
+    # tracking
+    mlflow_experiment: str
+    mlflow_run_name: str
+    artifact_category: str
+    artifact_name: str
+
+
+def load_model_config(config_name: str) -> ModelConfig:
+    """Loads model configuration from configs/models/
+
+    Parameters
+    ----------
+    config_name : str
+       name of the yaml configuration file
+       should be provided without .yaml
+
+    Returns
+    -------
+    ModelConfig
+        dataclass object with model configuration
+    """
+    cfg = yaml.safe_load(
+        Path(MODEL_CONFIG_DIR / f"{config_name}.yaml").read_text(
+            encoding="utf-8"
+        )
+    )
+
+    return ModelConfig(
+        train_set=(cfg["data"]["train_set"]),
+        dev_set=(cfg["data"]["dev_set"]),
+        test_set=(cfg["data"]["test_set"]),
+        id_col=cfg["data"]["id_col"],
+        time_col=cfg["data"]["time_col"],
+        model_type=cfg["model"]["type"],
+        strategy=cfg["model"]["strategy"],
+        horizon=cfg["model"]["horizon"],
+        target_col=cfg["target"]["col"],
+        features=cfg["features"]["list"],
+        model_params=cfg["lgbm_params"],
+        mlflow_experiment=cfg["tracking"]["mlflow_experiment"],
+        mlflow_run_name=cfg["tracking"]["mlflow_run_name"],
+        artifact_category=cfg["tracking"]["artifact_category"],
+        artifact_name=cfg["tracking"]["artifact_name"],
+    )
